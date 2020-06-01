33fa73a4a075494c8a510bfe88a990ba706b6ec7
Cleber Silva <kernel.conf@gmail.com>
2020-05-31 22:05:33 -0300

    WIP

--- src/main/java/br/com/consulta/cep/CepApplication.java
+++ src/main/java/br/com/consulta/cep/CepApplication.java
@@ -6,13 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CepApplication {
-
 	public static void main(String[] args) {
-
 		SpringApplication.run(CepApplication.class, args);
-
-
-
 	}
-
 }
